f84f86ab2bd0f08237eb72b7435304550966d97a
pvfigueroa-edigital <p_vfigueroa@enteldigital.cl>
2026-03-12 10:38:31 -0300

    Fix: Sincronizar import con requirements.txt en AWS

--- extractor-aws/main.py
+++ extractor-aws/main.py
@@ -1,7 +1,5 @@
 import requests
-import functions_framework 
 
-@functions_framework.http
 def ejecutar_recoleccion(request):
     print("Ejecutando extractor AWS de prueba...")
     return ("Inventario AWS extraído", 200)
