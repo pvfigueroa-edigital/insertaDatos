0a9312735f5a1d88ed9816eec12bc302db87eec5
pvfigueroa-edigital <p_vfigueroa@enteldigital.cl>
2026-03-12 10:44:34 -0300

    Fix: Agregando el decorador HTTP y framework a AWS

--- extractor-aws/main.py
+++ extractor-aws/main.py
@@ -1,5 +1,7 @@
+import functions_framework
 import requests
 
+@functions_framework.http
 def ejecutar_recoleccion(request):
     print("Ejecutando extractor AWS de prueba...")
     return ("Inventario AWS extraído", 200)
